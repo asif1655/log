4569aff29854351e6eb7a79f710a51e3de95eb61
Asif <82995648+asif1655@users.noreply.github.com>
2025-11-08 11:50:14 +0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// This file holds your ACTUAL, SECRET configuration. 
+// Add this file to your .gitignore!
+
+const YOUR_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCDoULyWPXO-kBwwTaRrQDscciScCh91hc",
+  authDomain: "tgrmcc-9dc96.firebaseapp.com",
+  projectId: "tgrmcc-9dc96",
+  storageBucket: "tgrmcc-9dc96.firebasestorage.app",
+  messagingSenderId: "1061433086609",
+  appId: "1:1061433086609:web:c2f4bf8d01ffa3e3986530",
+  measurementId: "G-VC3SDY1773"
+};
+
+// IMPORTANT: The global variable name must match what is used in index.html
+window.FIREBASE_CONFIG_CLIENT = YOUR_FIREBASE_CONFIG;
